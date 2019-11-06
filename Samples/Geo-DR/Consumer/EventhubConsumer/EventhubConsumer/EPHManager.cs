ac2f1090f39dc8ed32863f2a7afd469ec13192e5
Patrick Schuler <paschule@microsoft.com>
2019-11-06 14:01:13 -0800

    Initial

---
+++ Samples/Geo-DR/Consumer/EventhubConsumer/EventhubConsumer/EPHManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventhubConsumer
+{
+    public class EPHManager
+    {
+        private readonly string geoDrAliasConnectionString;
+        private readonly string storageConnectionString;
+        private readonly string[] storageContainers;
+
+        public EPHManager(string geoDrAliasConnectionString, string storageConnectionString, string[] storageContainers)
+        {
+            this.geoDrAliasConnectionString = geoDrAliasConnectionString;
+            this.storageConnectionString = storageConnectionString;
+            this.storageContainers = storageContainers;
+        }
+
+        public Task StartAsync(CancellationToken cts = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cts = default)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Samples/Geo-DR/Consumer/EventhubConsumer/EventhubConsumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EventhubConsumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+            var ephManager = new EPHManager("", "", null);
+            await ephManager.StartAsync();
+            
+            Console.WriteLine("Receiving. Press ENTER to stop worker.");
+            Console.ReadLine();
+
+            await ephManager.StopAsync();
+        }
+    }
+}
